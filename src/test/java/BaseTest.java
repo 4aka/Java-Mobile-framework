536a129c64f70687e669074d133674428d573ffe
4aka <7nuclear@gmail.com>
2024-12-11 13:20:13 +0200

    Add global var - tab

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,8 +1,11 @@
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import pages.AppTabs;
 
 
 public class BaseTest {
+
+    public AppTabs tab = new AppTabs();
 
     @BeforeClass
     public void setup() {
