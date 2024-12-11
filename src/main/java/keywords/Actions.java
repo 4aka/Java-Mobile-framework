0228ae09e45b606a0c9b02fd76565ca0a5b0b502
4aka <7nuclear@gmail.com>
2024-12-11 13:19:38 +0200

    Add clickByCoordinates()

--- src/main/java/keywords/Actions.java
+++ src/main/java/keywords/Actions.java
@@ -2,6 +2,13 @@ package keywords;
 
 import io.appium.java_client.AppiumBy;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Pause;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
 
 import static framework.InitDriver.getDriver;
 
@@ -12,4 +19,23 @@ public class Actions {
                 "new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView("
                 + "new UiSelector().textContains(\"" + text + "\"));"));
     }
+
+    public static void clickByCoordinates(int x, int y) {
+        implicitlyWait(2);
+        PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+
+        Sequence sequence = new Sequence(finger, 1)
+                .addAction(finger.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), x, y))
+                .addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
+                .addAction(new Pause(finger, Duration.ofMillis(150)))
+                .addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+        getDriver().perform(Collections.singletonList(sequence));
+    }
+
+    public static void implicitlyWait(int seconds) {
+        getDriver().manage().timeouts().implicitlyWait(seconds, TimeUnit.SECONDS);
+    }
+
+
 }
